9ab6995b701fe5a095f6fe39a00df2003929b9aa
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-06-15 18:14:11 -0300

    ...

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import TopBar from "../Bars/TopBar";
+
+function App() {
+    return(
+        <TopBar />
+    )
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom";
+import App from "./components/App/App";
+import "./index.css";
+// import { store } from './redux/store';
+// import { Provider } from 'react-redux';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')    
+);
