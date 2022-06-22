498ccca260f00cae2254b3a5a3ff575183f7462c
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-06-21 21:34:57 -0300

    Add the flex-none property to the profile picture container to prevent it from shrinking

--- src/components/Bars/TopBar.jsx
+++ src/components/Bars/TopBar.jsx
@@ -5,15 +5,15 @@ function TopBar() {
         <div className="md:w-8/12 flex items-center min-w-0 h-14 px-4 bg-gray-800">
             <span className="md:hidden material-icons material-icons-outlined text-gray-400">arrow_back</span>
             <div className="md:pl-0 flex grow min-w-0 pl-4">
-                <div className="w-11 h-11 rounded-full"><img className="w-full min-h-full object-cover rounded-full" src={userPic} alt="User" /></div>
+                <div className="flex-none w-11 h-11 rounded-full"><img className="w-full min-h-full object-cover rounded-full" src={userPic} alt="User" /></div>
                 <div className="min-w-0 pl-4">
                     <div className="min-w-0 font-medium text-white whitespace-nowrap overflow-hidden text-ellipsis">Lisa</div>
                     <div className="min-w-0 text-sm text-gray-400 whitespace-nowrap overflow-hidden text-ellipsis">last seen a long time ago</div>
                 </div>
             </div>
-            <div className="sm:w-36 sm:p-0 flex w-fit h-11 pl-3">
-                <div className="hidden sm:flex sm:justify-center sm:items-center sm:grow"><span className="material-icons material-icons-outlined text-gray-400">phone</span></div>
-                <div className="hidden sm:flex sm:justify-center sm:items-center sm:grow"><span className="material-icons material-icons-outlined text-gray-400">search</span></div>
+            <div className="sm:w-36 sm:p-0 flex w-fit h-11 pl-3 bg-gray-800">
+                <div className="sm:flex sm:justify-center sm:items-center sm:grow hidden"><span className="material-icons material-icons-outlined text-gray-400">phone</span></div>
+                <div className="sm:flex sm:justify-center sm:items-center sm:grow hidden"><span className="material-icons material-icons-outlined text-gray-400">search</span></div>
                 <div className="sm:justify-center flex justify-end items-center grow"><span className="material-icons material-icons-outlined text-gray-400">more_vert</span></div>
             </div>
         </div>
