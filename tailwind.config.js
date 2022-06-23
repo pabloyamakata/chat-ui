114d1a036ccf4977362d6836239f450c4a150633
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-06-23 17:44:53 -0300

    Add new custom property radial gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,11 @@ module.exports = {
     extend: {
       fontFamily: {
         'sans': ['roboto', ...defaultTheme.fontFamily.sans],
-      }
+      },
+      backgroundImage: {
+        'radial-gradient': 'radial-gradient(180% 180% at right -65% top -65%, transparent 99%, #1f2937)',
+      },
     },
   },
-  plugins: []
+  plugins: [],
 }
