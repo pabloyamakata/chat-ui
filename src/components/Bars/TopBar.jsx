01f3bdd3289ee3aa91547a4ef97a16ca343914e8
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-06-24 16:22:45 -0300

    Set the width of the component to 100% of its container

--- src/components/Bars/TopBar.jsx
+++ src/components/Bars/TopBar.jsx
@@ -2,7 +2,7 @@ import userPic from "../../assets/images/userpfp-1.jpg";
 
 function TopBar() {
     return(
-        <div className="md:w-8/12 flex items-center min-w-0 h-14 px-4 bg-gray-800">
+        <div className="flex items-center min-w-0 h-14 px-4 bg-gray-800">
             <span className="md:hidden material-icons material-icons-outlined text-gray-400">arrow_back</span>
             <div className="md:pl-0 flex grow min-w-0 pl-4">
                 <div className="flex-none w-11 h-11 rounded-full"><img className="w-full min-h-full object-cover rounded-full" src={userPic} alt="User" /></div>
