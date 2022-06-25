e05bbf2a361c2373f8cdda0e358b9030af577b48
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-06-25 14:37:58 -0300

    Add type attribute to button tags

--- src/components/Buttons/BasicButton.jsx
+++ src/components/Buttons/BasicButton.jsx
@@ -1,6 +1,6 @@
 function BasicButton({ btnDescription }) {
     return(
-        <button className="sm:w-[58px] sm:h-[58px] focus:ring active:scale-90 flex justify-center items-center w-[50px] h-[50px] bg-blue-500 ring-blue-300 rounded-full transition-transform">
+        <button type="button" className="sm:w-[58px] sm:h-[58px] focus:ring active:scale-90 flex justify-center items-center w-[50px] h-[50px] bg-blue-500 ring-blue-300 rounded-full transition-transform">
             <span className="sm:text-3xl text-[26px] text-white material-icons">{ btnDescription }</span>
         </button>
     )
