e45cc86592ac5806630f6c82f03ee0f922772139
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-06-27 20:58:51 -0300

    Rename component to RightBubble.jsx

---
+++ src/components/Bubbles/RightBubble.jsx
@@ -0,0 +1,16 @@
+function RightBubble() {
+    return(
+        <div className="relative flex max-w-[480px] py-[6px] pr-2 pl-[10px] bg-blue-500 rounded-xl rounded-br-none whitespace-pre-wrap break-words">
+            <p className="min-w-0 text-white">I'm doing one component at a time. This, for example, is a chat bubble.</p>
+            <div className="flex self-end">
+                <span className="text-xs font-medium text-gray-200">11:15</span>
+                <span className="hidden material-icons material-icons-outlined ml-1 text-xs text-gray-100">schedule</span>
+                <span className="hidden material-icons material-icons-outlined ml-1 text-[18px] text-gray-100">done</span>
+                <span className="material-icons material-icons-outlined ml-1 text-[18px] text-gray-100">done_all</span>
+            </div>
+            <div className="absolute bottom-0 -right-2 w-3 h-6 bg-blue-radial-gradient"></div>
+        </div>
+    )
+}
+
+export default RightBubble;
