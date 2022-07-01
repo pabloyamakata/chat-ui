1113c2a4efa332cb2e15ed9f10b82b1eb460ad57
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-01 18:32:39 -0300

    Generate jsx structure for the component

--- src/components/Users/ContactBox.jsx
+++ src/components/Users/ContactBox.jsx
@@ -1,6 +1,12 @@
-function ContactBox() {
+function ContactBox({ userPic }) {
     return(
-        <div></div>
+        <div className="flex min-w-0 h-[72px]">
+            <div className="flex-none w-11 h-11 rounded-full"><img className="w-full min-h-full object-cover rounded-full" src={userPic} alt="User" /></div>
+            <div className="min-w-0 pl-4">
+                <div className="min-w-0 font-medium text-white whitespace-nowrap overflow-hidden text-ellipsis">Lisa</div>
+                <div className="min-w-0 text-sm text-gray-400 whitespace-nowrap overflow-hidden text-ellipsis">last seen a long time ago</div>
+            </div>
+        </div>
     )
 }
 
