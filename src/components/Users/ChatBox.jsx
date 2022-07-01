b1216aa9a6e762f9299a08fa46c2328066902660
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-01 18:25:05 -0300

    Generate jsx structure for the component

--- src/components/Users/ChatBox.jsx
+++ src/components/Users/ChatBox.jsx
@@ -1,6 +1,18 @@
 function ChatBox() {
     return(
-        <div></div>
+        <a className="block h-[72px] p-[9px] bg-gray-800">
+            <div><img /></div>
+            <div>
+                <p>
+                    <span>Lisa Aureliana</span>
+                    <span>14:26</span>
+                </p>
+                <p>
+                    <span>This should be the user's last message</span>
+                    <div>53</div>
+                </p>
+            </div>
+        </a>
     )
 }
 
