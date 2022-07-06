e1c09a4ed15e3b543bbe5802eb4a220fd67cdee0
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-05 22:13:11 -0300

    Style the component

--- src/components/Users/ChatBox.jsx
+++ src/components/Users/ChatBox.jsx
@@ -1,15 +1,15 @@
 function ChatBox({ userPic }) {
     return(
         // Add a min-width to this component and overflow-hidden class to its parent to make it hide when resizing the browser
-        <a className="hover:bg-slate-700 flex items-center min-w-[280px] h-[72px] p-[9px] bg-gray-800">
+        <a className="hover:bg-slate-700 flex items-center min-w-[250px] h-[72px] p-[9px] bg-gray-800">
             <div className="flex-none w-[54px] h-[54px] rounded-full"><img className="w-full min-h-full object-cover rounded-full" src={userPic} alt="User" /></div>
-            <div className="min-w-0 grow pl-2 pr-1">
-                <p className="flex justify-between items-center min-w-0">
-                    <span className="min-w-0 pr-3 font-medium text-white whitespace-nowrap overflow-hidden text-ellipsis">Lisa Aureliana</span>
+            <div className="grow min-w-0 pl-2 pr-1">
+                <p className="flex justify-between items-center">
+                    <span className="pr-3 font-medium text-white whitespace-nowrap overflow-hidden text-ellipsis">Lisa Aureliana</span>
                     <span className="text-xs font-medium text-gray-400">09/10/2021</span>
                 </p>
-                <p className="flex justify-between items-center min-w-0">
-                    <span className="min-w-0 pr-3 text-[15px] leading-6 text-gray-400 whitespace-nowrap overflow-hidden text-ellipsis">This should be the user's last message</span>
+                <p className="flex justify-between items-center">
+                    <span className="pr-3 text-[15px] leading-6 text-gray-400 whitespace-nowrap overflow-hidden text-ellipsis">This should be the user's last message</span>
                     <span className="py-px px-2 text-sm font-medium text-white bg-gray-500 rounded-full">1541</span>
                 </p>
             </div>
