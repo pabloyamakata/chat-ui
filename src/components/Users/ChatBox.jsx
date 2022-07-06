7af2b0c8b616cc62bd020b8c276c6b90c9656893
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-05 23:14:49 -0300

    Style the component

--- src/components/Users/ChatBox.jsx
+++ src/components/Users/ChatBox.jsx
@@ -1,7 +1,7 @@
 function ChatBox({ userPic }) {
     return(
         // Add a min-width to this component and overflow-hidden class to its parent to make it hide when resizing the browser
-        <a className="hover:bg-slate-700 flex items-center min-w-[250px] h-[72px] p-[9px] bg-gray-800">
+        <a className="hover:bg-slate-700 flex items-center min-w-[250px] h-[72px] p-[9px] bg-gray-800 rounded-lg cursor-pointer tap-highlight-none">
             <div className="flex-none w-[54px] h-[54px] rounded-full"><img className="w-full min-h-full object-cover rounded-full" src={userPic} alt="User" /></div>
             <div className="grow min-w-0 pl-2 pr-1">
                 <p className="flex justify-between items-center">
