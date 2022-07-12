145e417b29524a533984a83989a001af786c0b9b
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-12 16:06:13 -0300

    Style component

--- src/components/Inputs/SearchInput.jsx
+++ src/components/Inputs/SearchInput.jsx
@@ -1,6 +1,9 @@
 function SearchInput() {
     return(
-        <div></div>
+        <div className="relative inline-block w-full">
+            <span class="material-icons material-icons-outlined absolute top-2/4 left-3 -translate-y-2/4 text-3xl text-gray-400">search</span>
+            <input className="focus:text-gray-200 focus:bg-gray-800 focus:outline-0 w-full h-11 pb-[3px] pl-[49px] pr-11 text-gray-400 bg-gray-900 rounded-full caret-gray-400" placeholder="Search" />
+        </div>
     )
 }
 
