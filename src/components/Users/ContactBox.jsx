0214e5f5b6b6f81da716d80b181e7b299e6c59ee
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-12 09:53:19 -0300

    Style component

--- src/components/Users/ContactBox.jsx
+++ src/components/Users/ContactBox.jsx
@@ -1,12 +1,13 @@
 function ContactBox({ userPic }) {
     return(
-        <div className="flex min-w-0 h-[72px]">
-            <div className="flex-none w-11 h-11 rounded-full"><img className="w-full min-h-full object-cover rounded-full" src={userPic} alt="User" /></div>
-            <div className="min-w-0 pl-4">
-                <div className="min-w-0 font-medium text-white whitespace-nowrap overflow-hidden text-ellipsis">Lisa</div>
-                <div className="min-w-0 text-sm text-gray-400 whitespace-nowrap overflow-hidden text-ellipsis">last seen a long time ago</div>
+        // Add a min-width to this component and overflow-hidden class to its parent to make it hide when resizing the browser
+        <a className="hover:bg-slate-700 flex items-center min-w-[250px] h-[72px] p-[9px] bg-gray-800 rounded-lg cursor-pointer tap-highlight-none transition-[background-color]">
+            <div className="flex-none w-[54px] h-[54px] rounded-full"><img className="w-full min-h-full object-cover rounded-full" src={userPic} alt="User" /></div>
+            <div className="grow min-w-0 pl-2 pr-1">
+                <p className="pr-3 font-medium text-white whitespace-nowrap overflow-hidden text-ellipsis">Lisa Aureliana</p>
+                <p className="pr-3 text-sm text-gray-400 whitespace-nowrap overflow-hidden text-ellipsis">last seen a long time ago</p>
             </div>
-        </div>
+        </a>
     )
 }
 
