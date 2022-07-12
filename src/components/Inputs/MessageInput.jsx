315ac938d808cd72b8fde28097075fded1f37ba3
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-12 16:03:04 -0300

    Add type attribute to input and change the color of its cursor when is focused

--- src/components/Inputs/MessageInput.jsx
+++ src/components/Inputs/MessageInput.jsx
@@ -2,7 +2,7 @@ function MessageInput() {
     return(
         <div className="relative flex items-center h-12 pt-1 px-3 rounded-xl rounded-br-none bg-gray-800">
             <button type="button"><span className="material-icons material-icons-outlined text-gray-400">sentiment_satisfied_alt</span></button>
-            <input placeholder="Message" className="focus:text-gray-200 focus:outline-0 grow min-w-0 mb-1.5 pl-3 text-gray-400 bg-transparent whitespace-nowrap overflow-hidden text-ellipsis" />
+            <input type="text" placeholder="Message" className="focus:text-gray-200 focus:outline-0 grow min-w-0 mb-1.5 pl-3 text-gray-400 bg-transparent caret-gray-400 whitespace-nowrap overflow-hidden text-ellipsis" />
             <button type="button" className="pl-3"><span className="material-icons material-icons-outlined text-gray-400 rotate-45">attach_file</span></button>
             <div className="absolute bottom-0 -right-2 w-3 h-6 bg-gray-radial-gradient"></div>
         </div>
