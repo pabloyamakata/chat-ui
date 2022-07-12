fbf1da3cb7392a08d501d677fafe37c6a15e0717
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-12 09:50:34 -0300

    Import ContactBox component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,11 @@
-import MessageInput from "../Inputs/MessageInput";
+import ContactBox from "../Users/ContactBox";
+import userPic from "../../assets/images/userpfp-1.jpg";
 
 function App() {
     return(
-        <MessageInput />
+        <div className="overflow-hidden">
+            <ContactBox userPic={userPic} />
+        </div>
     )
 }
 
