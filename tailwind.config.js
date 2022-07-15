8743f52abbcbe12dd1ed2115cddbf8e1bddbe710
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-15 18:25:23 -0300

    style: Create new class

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         'gray-radial-gradient': 'radial-gradient(180% 180% at right -65% top -65%, transparent 99%, #1f2937)',
         'blue-radial-gradient': 'radial-gradient(180% 180% at right -65% top -65%, transparent 99%, #3b82f6)',
       },
+      boxShadow: {
+        'inset': 'inset 0 0 0 2px #1d4ed8',
+      },
     },
   },
   plugins: [],
