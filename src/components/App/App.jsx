fe69d7c46289c7e861bc701c6f60e6adb8c7afef
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-15 18:20:19 -0300

    Style component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,7 @@ import MessageInput from "../Inputs/MessageInput";
 
 function App() {
     return(
-        <div className="m-4">
+        <div className="p-4 overflow-hidden">
             <SearchInput />
             <MessageInput />
         </div>
