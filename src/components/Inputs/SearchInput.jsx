210b85b7e0585b2221d333a420b70c2297e497fe
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-15 23:57:22 -0300

    style: Add cursor-pointer class to close/reset icon

--- src/components/Inputs/SearchInput.jsx
+++ src/components/Inputs/SearchInput.jsx
@@ -9,7 +9,7 @@ function SearchInput() {
         <div className="relative inline-block min-w-[175px] w-full">
             <input onChange={(event) => setInputValue(event.target.value)} value={inputValue} className="focus:text-gray-200 focus:shadow-inset w-full h-11 pb-[3px] pl-[49px] pr-11 text-gray-400 bg-gray-900 rounded-full outline-0 caret-blue-500 transition-all peer" placeholder="Search" />
             <span className="material-icons material-icons-outlined absolute top-2/4 left-3 -translate-y-2/4 text-3xl text-gray-400 peer-focus:text-blue-500">search</span>
-            <span onClick={() => setInputValue('')} className={`${ !inputValue.length && 'hidden' } material-icons material-icons-outlined absolute top-2/4 right-3 -translate-y-2/4 text-2xl text-gray-400 peer-focus:text-blue-500`}>close</span>
+            <span onClick={() => setInputValue('')} className={`${ !inputValue.length && 'hidden' } material-icons material-icons-outlined absolute top-2/4 right-3 -translate-y-2/4 text-2xl text-gray-400 cursor-pointer peer-focus:text-blue-500`}>close</span>
         </div>
     )
 }
