c22784342df86ecace7f97f212d59f14ea34f3d2
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-16 20:55:58 -0300

    Import SearchBar.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,9 @@
-import SearchInput from "../Inputs/SearchInput";
-import MessageInput from "../Inputs/MessageInput";
+import SearchBar from "../Bars/SearchBar";
 
 function App() {
     return(
-        <div className="p-4 overflow-hidden">
-            <SearchInput />
-            <MessageInput />
+        <div className="overflow-hidden">
+            <SearchBar />
         </div>
     )
 }
