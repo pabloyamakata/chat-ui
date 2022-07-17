68687b4f0820c1478c00f51162bf31e57020859b
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-16 21:13:09 -0300

    Style component

--- src/components/Bars/SearchBar.jsx
+++ src/components/Bars/SearchBar.jsx
@@ -1,6 +1,16 @@
+import SearchInput from "../Inputs/SearchInput";
+
 function SearchBar() {
     return(
-        <div></div>
+        // This component's parent must have the overflow-hidden class applied. No need to add min-width
+        <div className="p-2 bg-gray-800">
+            <div className="flex items-center">
+                <div className="flex items-center pt-[5px] pr-[5px] pb-[8px] pl-[8px]">
+                    <span className="material-icons material-icons-outlined text-gray-400">menu</span>
+                </div>
+                <div className="grow ml-[15px]"><SearchInput /></div>
+            </div>
+        </div>
     )
 }
 
