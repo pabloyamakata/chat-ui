7833dfebe1cfde208db4c348ee1cd0b35b77532d
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-16 21:46:11 -0300

    Add arrow-back icon

--- src/components/Bars/SearchBar.jsx
+++ src/components/Bars/SearchBar.jsx
@@ -6,6 +6,7 @@ function SearchBar() {
         <div className="p-2 bg-gray-800">
             <div className="flex items-center">
                 <div className="flex items-center pt-[5px] pr-[5px] pb-[8px] pl-[8px]">
+                    <span className="hidden material-icons material-icons-outlined text-[27px] text-gray-400">arrow_back</span>
                     <span className="material-icons material-icons-outlined text-gray-400">menu</span>
                 </div>
                 <div className="grow ml-[15px]"><SearchInput /></div>
