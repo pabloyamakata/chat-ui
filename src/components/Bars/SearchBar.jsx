95dd1babcbc8f9b46c9f665db8c4b49bb9e5a31c
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-18 18:55:02 -0300

    feat: add redux logic
    Arrow-back icon will appear everytime user focus on the search input. As long as the search is activated, the arrow-back icon will persist

--- src/components/Bars/SearchBar.jsx
+++ src/components/Bars/SearchBar.jsx
@@ -1,13 +1,20 @@
+import { useSelector, useDispatch } from "react-redux";
+import { setIsSearchActive } from "../../redux/features/searchBar/searchBarSlice";
+
 import SearchInput from "../Inputs/SearchInput";
 
 function SearchBar() {
+
+    const isSearchActive = useSelector(state => state.searchBar.isSearchActive);
+    const dispatch = useDispatch();
+
     return(
         // This component's parent must have the overflow-hidden class applied. No need to add min-width
         <div className="p-2 bg-gray-800">
             <div className="flex items-center">
                 <div className="flex items-center pt-[5px] pr-[5px] pb-[8px] pl-[8px]">
-                    <span className="hidden material-icons material-icons-outlined text-[27px] text-gray-400">arrow_back</span>
-                    <span className="material-icons material-icons-outlined text-gray-400">menu</span>
+                    <span onClick={() => dispatch(setIsSearchActive())} className={`${!isSearchActive && 'hidden'} material-icons material-icons-outlined text-[27px] text-gray-400`}>arrow_back</span>
+                    <span className={`${isSearchActive && 'hidden'} material-icons material-icons-outlined text-gray-400`}>menu</span>
                 </div>
                 <div className="grow ml-[15px]"><SearchInput /></div>
             </div>
--- src/components/Inputs/SearchInput.jsx
+++ src/components/Inputs/SearchInput.jsx
@@ -1,13 +1,22 @@
+import { useSelector, useDispatch } from "react-redux";
+import { setIsSearchActive } from "../../redux/features/searchBar/searchBarSlice";
+
 import { useState } from "react";
 
 function SearchInput() {
     
     const [inputValue, setInputValue] = useState('');
+    const isSearchActive = useSelector(state => state.searchBar.isSearchActive);
+    const dispatch = useDispatch();
+
+    const activateSearch = () => {
+        !isSearchActive && dispatch(setIsSearchActive());
+    };
 
     return(
         // Add a min-width to this component and overflow-hidden class to its parent to make it hide when resizing the browser
         <div className="relative min-w-[175px]">
-            <input onChange={(event) => setInputValue(event.target.value)} value={inputValue} className="focus:text-gray-200 focus:shadow-inset w-full h-11 pb-[3px] pl-[49px] pr-11 text-gray-400 bg-gray-900 rounded-full outline-0 caret-blue-500 transition-all peer" placeholder="Search" />
+            <input onFocus={activateSearch} onChange={(event) => setInputValue(event.target.value)} value={inputValue} className="focus:text-gray-200 focus:shadow-inset w-full h-11 pb-[3px] pl-[49px] pr-11 text-gray-400 bg-gray-900 rounded-full outline-0 caret-blue-500 transition-all peer" placeholder="Search" />
             <span className="material-icons material-icons-outlined absolute top-2/4 left-3 -translate-y-2/4 text-3xl text-gray-400 peer-focus:text-blue-500">search</span>
             <span onClick={() => setInputValue('')} className={`${ !inputValue.length && 'hidden' } material-icons material-icons-outlined active:text-blue-500 hover:rotate-90 hover:scale-125 absolute top-2/4 right-3 -translate-y-2/4 text-2xl text-gray-400 cursor-pointer transition-transform duration-300 tap-highlight-none peer-focus:text-blue-500`}>close</span>
         </div>
