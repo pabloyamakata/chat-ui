a7e1b67194eb4d678a93809fe366c8a13f747184
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-19 15:42:32 -0300

    feat: reset input value when user clicks on arrow-back icon

--- src/components/Bars/SearchBar.jsx
+++ src/components/Bars/SearchBar.jsx
@@ -1,19 +1,24 @@
 import { useSelector, useDispatch } from "react-redux";
-import { setIsSearchActive } from "../../redux/features/searchBar/searchBarSlice";
+import { setIsSearchActive, setInputValue } from "../../redux/features/search/searchSlice";
 
 import SearchInput from "../Inputs/SearchInput";
 
 function SearchBar() {
 
-    const isSearchActive = useSelector(state => state.searchBar.isSearchActive);
+    const isSearchActive = useSelector(state => state.search.isSearchActive);
     const dispatch = useDispatch();
+
+    const deactivateSearch = () => {
+        dispatch(setIsSearchActive());
+        dispatch(setInputValue(''));
+    };
 
     return(
         // This component's parent must have the overflow-hidden class applied. No need to add min-width
         <div className="p-2 bg-gray-800">
             <div className="flex items-center">
                 <div className="flex items-center pt-[5px] pr-[5px] pb-[8px] pl-[8px]">
-                    <span onClick={() => dispatch(setIsSearchActive())} className={`${!isSearchActive && 'hidden'} material-icons material-icons-outlined text-[25px] text-gray-400 cursor-pointer`}>arrow_back</span>
+                    <span onClick={deactivateSearch} className={`${!isSearchActive && 'hidden'} material-icons material-icons-outlined text-[25px] text-gray-400 cursor-pointer`}>arrow_back</span>
                     <span className={`${isSearchActive && 'hidden'} material-icons material-icons-outlined text-gray-400 cursor-pointer`}>menu</span>
                 </div>
                 <div className="grow ml-[15px]"><SearchInput /></div>
