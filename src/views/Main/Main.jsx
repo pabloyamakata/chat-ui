9b95e2b99cea992041d8084836ca904e980a0459
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-17 19:20:41 -0300

    style: make search bar sticky

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -38,7 +38,9 @@ function Main() {
     return(
         <div className='grid grid-cols-12 h-screen'>
             <div className='col-span-3 overflow-y-auto bg-slate-800'>
-                <SearchBar />
+                <div className='sticky top-0'>
+                    <SearchBar />
+                </div>
                 <div className='py-1 pr-1 pl-2'>
                     {contacts.map(contact => {
                         return(
