2fe0710120e00b15d0a33a3ddcc62d003c14db68
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-19 18:20:41 -0300

    style: change width, height and font size

--- src/components/Buttons/BasicButton.jsx
+++ src/components/Buttons/BasicButton.jsx
@@ -1,7 +1,7 @@
 function BasicButton({ btnDescription }) {
     return(
-        <button type="button" className="sm:w-[58px] sm:h-[58px] focus:ring active:scale-90 flex justify-center items-center w-[50px] h-[50px] bg-blue-500 ring-blue-300 rounded-full transition-transform tap-highlight-none">
-            <span className="sm:text-3xl text-[26px] text-white material-icons">{ btnDescription }</span>
+        <button type="button" className="focus:ring active:scale-90 flex justify-center items-center w-[52px] h-[52px] bg-blue-500 ring-blue-300 rounded-full transition-transform tap-highlight-none">
+            <span className="text-2xl text-white material-icons">{ btnDescription }</span>
         </button>
     )
 }
