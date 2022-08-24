e1505f750e1a9cc2c2478b376c49ce8018565efc
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-24 18:31:46 -0300

    feat: create jsx structure

---
+++ src/views/Login/Login.jsx
@@ -0,0 +1,26 @@
+function Login() {
+    return(
+        <div>
+            <div>
+                <div>Sign In</div>
+                <div>
+                    <div>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <button type="button">Login</button>
+                    </div>
+                </div>
+                <div>
+                    <span>Don't have an account?<a href="/">Create one</a></span>
+                </div>
+            </div>
+            <footer></footer>
+        </div>
+    )
+}
+
+export default Login;
